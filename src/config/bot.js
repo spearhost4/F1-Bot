42103c6dfad3e9b586345d9fa5e8b5e79af267bb
spearhost4 <spearhost4@gmail.com>
2026-05-04 13:08:44 +0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "F1ʙᴅ᾽ѕ Personal Bot🤖",
         // Activity type number (0 = Playing).
         type: 0, 
       },
